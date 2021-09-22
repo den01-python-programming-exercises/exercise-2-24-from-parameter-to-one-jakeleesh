d427f4e4f9c6a6debbd7d3787543aeee90199bc0
Jake Lee <jake.lee@fdmgroup.com>
2021-09-22 22:22:54 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    num = int(input())
+    print_from_number_to_one(num)
+
+def print_from_number_to_one(num):
+    while (num > 0):
+        print(num)
+        num -= 1
 
 if __name__ == '__main__':
     main()
